79c525af1712c965620e9f355e43753d71062006
Yadunandan Batchu <nandubatchu@Yadunandans-MacBook-Pro.local>
2023-07-28 04:30:53 +0530

    update retry count to 20

--- dbt/adapters/glue/gluedbapi/connection.py
+++ dbt/adapters/glue/gluedbapi/connection.py
@@ -135,7 +135,7 @@ class GlueConnection:
     def client(self):
         config = Config(
             retries={
-                'max_attempts': 10,
+                'max_attempts': 20,
                 'mode': 'adaptive'
             }
         )
